abd383e15ab0f35a016024911057fb15968d8f2c
Christopher Norton <cnorton1@gmail.com>
2016-11-21 22:38:43 -0500

    Remove unecessary files

--- app/app.jsx
+++ app/app.jsx
@@ -1,11 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router'); // <---- Destructuring Syntax
-// OR 
-// var Route = require('react-router').Route;
-// var Router = require('react-router').Router;
-// var IndexRoute = require('react-router').IndexRoute;
-// var hashHistory = require('react-router').hashHistory;
+
 var Main = require('Main');
 var Nav = require('Nav');
 var Weather = require('Weather');
