b9ec6cba93fb14a637688bade65a55bb5c34fb47
Christopher Norton <cnorton1@gmail.com>
2016-11-23 23:54:42 -0500

    Styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,17 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
     return (
             <div>
-                <h3>About</h3>
-                <p>Welcome to the About page!</p>
+                <h1 className="text-center">About</h1>
+                <p>This a weather app built using React</p>
+                <p>Some of the tools used for this app:</p>
+                <ul>
+                    <li><a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.</li>
+                    <li><a href="http://openweathermap.org">Open Weather Map</a> - This was used to search for weather data by city name.</li>
+                    <li><a href="https://github.com/cdnorton1">GitHub</a> - This is where I have my code stored.</li>
+                </ul>
             </div>
         )
 };
